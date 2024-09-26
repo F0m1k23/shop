3c9c68aeb47455bd86e30ea9f700ade3a4242508
Vitalii <159725221+F0m1k23@users.noreply.github.com>
2024-09-26 19:40:58 +0200

    Delete js/script.js

--- js/script.js
+++
@@ -1,7 +0,0 @@
-import { headers } from "./headerMenu.js";
-const buttonOpen = document.getElementById('button')
-
-buttonOpen.addEventListener('click', () => {
-  getAllert();
-});
-// Set up the alert service
