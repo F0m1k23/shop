67dcf57f9c43fda29e606af65fc7a24e55305f7f
Vitalii <159725221+F0m1k23@users.noreply.github.com>
2024-09-26 19:39:58 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+import { headers } from "./headerMenu.js";
+const buttonOpen = document.getElementById('button')
+
+buttonOpen.addEventListener('click', () => {
+  getAllert();
+});
+// Set up the alert service
